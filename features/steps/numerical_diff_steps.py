41fcd169be78a5aebd997e999a6df7fe9ac27230
Alexey Gribchenko <gribchenko.2003@mail.ru>
2023-10-12 14:14:30 +0300

    Fixed test for function numerical_diff

--- features/steps/numerical_diff_steps.py
+++ features/steps/numerical_diff_steps.py
@@ -1,16 +1,19 @@
 from behave import given, when, then
 from functions import *
 
-@given('функция f(x) "{f}", точка x "{x:d}" и шаг h "{h:e}"')
+
+@given('функция f(x) "{f}", точка x "{x}" и шаг h "{h}"')
 def step_impl(context, f, x, h):
-    context.f = f
-    context.x = x
-    context.h = h
+    context.f = eval(f)
+    context.x = float(x)
+    context.h = float(h)
+
 
 @when('я вычисляю производную в этой точке')
 def step_impl(context):
     context.result = numerical_diff(context.f, context.x, context.h)
 
-@then('я получаю значение близкое к "{expected_result:f}"')
+
+@then('я получаю значение близкое к "{expected_result}"')
 def step_impl(context, expected_result):
-    assert abs(context.result - expected_result) < 1e-6
+    assert abs(context.result - float(expected_result)) < 1e-6
